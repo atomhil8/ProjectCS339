4caf2174bd2c2855fd2c3da862a65d5d0ba08dfc
Luke Werner <luke.werner2005@gmail.com>
2026-04-28 23:41:12 -0500

    Update executor.h

--- executor.h
+++ executor.h
@@ -1,19 +1,26 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
 
-//#include "instruction.h"
-#include "machine_state.cpp"
+#include "machine_state.h"
 #include "operations.h"
+
 #include <string>
 #include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
 
 // Rids string of comma
 std::string clean(std::string s);
 
 // Breaks down entire program into computer-friendly vector, also documents labels
-std::vector<std::string> parse(const std::string& fileName, MachineState& state);
+std::vector<std::string> parse(const std::string& fileName, ::MachineState& state);
+
+void IFStage(const std::vector<std::string> program, ::MachineState& state);
 
 // Executes one instruction line
-void execute(const std::string& instruction, MachineState& state, const bool& debug);
+void execute(const std::string& instruction, ::MachineState& state, const bool& debug);
 
 #endif
