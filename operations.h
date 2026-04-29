944d2d55b4baa423b9be4f9e6874000c4133691f
Luke Werner <luke.werner2005@gmail.com>
2026-04-28 23:44:16 -0500

    Update operations.h

--- operations.h
+++ operations.h
@@ -1,10 +1,11 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
-#include "instruction.h"
 #include "machine_state.h"
+
 #include <unordered_map>
 #include <string>
+#include <stdexcept>
 
 int op_add(int a, int b);
 int op_addi(int a, int imm);
@@ -19,7 +20,7 @@ int op_lw(MachineState& state, int base, int offset);
 void op_sw(MachineState& state, int base, int offset, int value);
 
 bool op_beq(int a, int b);
-int op_j(const unordered_map<string, int>& label_table, const string& label);
+int op_j(const std::unordered_map<std::string, int>& label_table, const std::string& label);
 
 void enforce_zero_register(MachineState& state);
 
