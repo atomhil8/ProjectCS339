b50eea3857ae7a401604fe9047e1d70bd79da423
Luke Werner <luke.werner2005@gmail.com>
2026-04-29 07:18:47 -0500

    Update executor.cpp

--- executor.cpp
+++ executor.cpp
@@ -1,5 +1,5 @@
 // Luke Werner A05265888 onn16
-// 4/27/2026
+// 4/30/2026
 // executor.cpp
 // Parses input and executes instructions
 
@@ -50,12 +50,18 @@ std::vector<std::string> parse(const std::string& fileName, MachineState& state)
     return program;
 }
 
-void IFStage(const std::vector<std::string> program, MachineState& state) {
+void IFStage(const std::vector<std::string>& program, MachineState& state) {
+    state.if_id = state.next_if_id;
 
+    state.next_if_id.instruction = program[state.PC];
+    state.next_if_id.newPC = state.PC + 1;
 }
 
-void execute(const std::string& instruction, MachineState& state, const bool& debug) {
-    std::stringstream ss(instruction);
+void IDStage(MachineState& state) {
+    state.id_ex = state.next_id_ex;
+
+    // Fetch
+    std::stringstream ss(state.if_id.instruction);
 
     std::string op, a, b, c;
     ss >> op >> a >> b >> c;
@@ -64,199 +70,341 @@ void execute(const std::string& instruction, MachineState& state, const bool& de
     b = clean(b);
     c = clean(c);
 
-    std::cout << "Executing: " << instruction << std::endl;
+    // Clear old values
+    state.next_id_ex = {};
+    int rsVal = 0;
+    int rtVal = 0;
+    int imm = 0;
+    int rs = -1;
+    int rt = -1;
+    int rd = -1;
 
-    if(op == "ADD" || op == "Add" || op == "add") {
-        // Assuming ctrl uses RegDst, RegWrite, ALUSrc, MemWrite, MemRead, MemtoReg, PCSrc
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
+    // Decode instructions
+    if(op == "add" || op == "Add" || op == "ADD" ||
+        op == "sub" || op == "Sub" || op == "SUB" ||
+        op == "and" || op == "And" || op == "AND" ||
+        op == "mul" || op == "Mul" || op == "MUL" ||
+        op == "or" || op == "Or" || op == "OR") {
 
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = state.REG[state.regMap.at(c)];
+        rd = state.regMap.at(a);
+        rs = state.regMap.at(b);
+        rt = state.regMap.at(c);
 
-        int sum = op_add(arg1, arg2);
+        rsVal = state.REG[rs];
+        rtVal = state.REG[rt];
 
-        state.writeReg(writeID, sum);
+        state.next_id_ex.RegDst = true;
+        state.next_id_ex.RegWrite = true;
+        state.next_id_ex.ALUSrc = false;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
     }
-    else if(op == "ADDI" || op == "Addi" || op == "addi") {
-        state.ctrl = {0, 1, 1, 0, 0, 0, 0};
+    else if(op == "addi" || op == "Addi" || op == "ADDI") {
 
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = stoi(c);
+        rt = state.regMap.at(a);
+        rs = state.regMap.at(b);
+        imm = std::stoi(c);
 
-        int sum = op_addi(arg1, arg2);
+        rsVal = state.REG[rs];
 
-        state.writeReg(writeID, sum);
+        state.next_id_ex.RegDst = false;
+        state.next_id_ex.RegWrite = true;
+        state.next_id_ex.ALUSrc = true;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
     }
-    else if(op == "SUB" || op == "Sub" || op == "sub") {
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
+    else if(op == "sll" || op == "Sll" || op == "SLL") {
 
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = state.REG[state.regMap.at(c)];
+        rd = state.regMap.at(a);
+        rt = state.regMap.at(b);
+        // shamt
+        imm = std::stoi(c);
 
-        int diff = op_sub(arg1, arg2);
+        rtVal = state.REG[rt];
 
-        state.writeReg(writeID, diff);
+        state.next_id_ex.RegDst = true;
+        state.next_id_ex.RegWrite = true;
+        state.next_id_ex.ALUSrc = false;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
     }
-    else if(op == "MUL" || op == "Mul" || op == "mul") {
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
+    else if(op == "srl" || op == "Srl" || op == "SRL") {
 
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = state.REG[state.regMap.at(c)];
+        rd = state.regMap.at(a);
+        rt = state.regMap.at(b);
+        // shamt
+        imm = std::stoi(c);
 
-        int prod = op_mul(arg1, arg2);
+        rtVal = state.REG[rt];
 
-        state.writeReg(writeID, prod);
+        state.next_id_ex.RegDst = true;
+        state.next_id_ex.RegWrite = true;
+        state.next_id_ex.ALUSrc = true;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
     }
+    else if(op == "lw" || op == "Lw" || op == "LW") {
 
-    else if(op == "AND" || op == "And" || op == "and") {
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
+        rt = state.regMap.at(a);
 
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = state.REG[state.regMap.at(c)];
-
-        int result = op_and(arg1, arg2);
-
-        state.writeReg(writeID, result);
-    }
-    else if(op == "OR" || op == "Or" || op == "or") {
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
-
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = state.REG[state.regMap.at(c)];
-
-        int result = op_or(arg1, arg2);
-
-        state.writeReg(writeID, result);
-    }
-    else if(op == "SLL" || op == "Sll" || op == "sll") {
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
-
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = stoi(c);
-
-        int result = op_sll(arg1, arg2);
-
-        state.writeReg(writeID, result);
-    }
-    else if(op == "SRL" || op == "Srl" || op == "srl") {
-        state.ctrl = {1, 1, 0, 0, 0, 0, 0};
-
-        int writeID = state.regMap.at(a);
-        int arg1 = state.REG[state.regMap.at(b)];
-        int arg2 = stoi(c);
-
-        int result = op_srl(arg1, arg2);
-
-        state.writeReg(writeID, result);
-    }
-
-    else if(op == "LW" || op == "Lw" || op == "lw") {
-        state.ctrl = {0, 1, 1, 0, 1, 1, 0};
-
-        int writeID = state.regMap.at(a);
-        
+        // Format: offset(base)
         std::size_t p1 = b.find('(');
         std::size_t p2 = b.find(')');
 
-        int offset = stoi(b.substr(0, p1));
-        // op_lw takes the base argument in terms of the address
-        int base = state.regMap.at(b.substr(p1 + 1, p2 - p1 - 1));
+        imm = std::stoi(b.substr(0, p1));
+        rs = state.regMap.at(b.substr(p1 + 1, p2 - p1 - 1));
 
-        int result = op_lw(state, base, offset);
+        rsVal = state.REG[rs];
 
-        state.writeReg(writeID, result);
+        state.next_id_ex.RegDst = true;
+        state.next_id_ex.RegWrite = true;
+        state.next_id_ex.ALUSrc = true;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = true;
+        state.next_id_ex.MemtoReg = true;
+        state.next_id_ex.PCSrc = false;
     }
-    else if(op == "SW" || op == "Sw" || op == "sw") {
-        state.ctrl = {0, 0, 1, 1, 0, 0, 0};
+    else if(op == "sw" || op == "Sw" || op == "SW") {
 
-        int storeVal = state.REG[state.regMap.at(a)];
-        
+        rt = state.regMap.at(a);
+
         std::size_t p1 = b.find('(');
         std::size_t p2 = b.find(')');
 
-        int offset = stoi(b.substr(0, p1));
-        // op_sw takes the base argument in terms of the address
-        int base = state.regMap.at(b.substr(p1 + 1, p2 - p1 - 1));
+        imm = std::stoi(b.substr(0, p1));
+        rs = state.regMap.at(b.substr(p1 + 1, p2 - p1 - 1));
 
-        op_sw(state, base, offset, storeVal);
-    }
+        rsVal = state.REG[rs];
+        rtVal = state.REG[rt];
 
-    ///// ALU for branch instructions does not align with machine state info, fix operations.cpp /////
-    else if(op == "BEQ" || op == "Beq" || op == "beq") {
-        state.ctrl = {0, 0, 0, 0, 0, 0, 1};
+        state.next_id_ex.RegDst = true;
+        state.next_id_ex.RegWrite = false;
+        state.next_id_ex.ALUSrc = true;
+        state.next_id_ex.MemWrite = true;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
     }
-    else if(op == "J" || op == "j") {
-        state.ctrl = {0, 0, 0, 0, 0, 0, 0};
-    }
+    else if(op == "beq" || op == "Beq" || op == "BEQ") {
 
-    else if(op == "NOP" || op == "Nop" || op == "nop") {
-        state.ctrl = {0, 0, 0, 0, 0, 0, 0};
-    }
+        rs = state.regMap.at(a);
+        rt = state.regMap.at(b);
+        imm = state.labelMap.at(c);
 
+        rsVal = state.REG[rs];
+        rtVal = state.REG[rt];
+
+        state.next_id_ex.RegDst = false;
+        state.next_id_ex.RegWrite = false;
+        state.next_id_ex.ALUSrc = false;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = true;
+    }
+    else if(op == "j" || op == "J") {
+
+        imm = state.labelMap.at(a);
+
+        state.next_id_ex.RegDst = false;
+        state.next_id_ex.RegWrite = false;
+        state.next_id_ex.ALUSrc = false;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
+
+        // state.id_ex.Jump = 1;
+    }
+    else if(op == "nop" || op == "Nop" || op == "NOP") {
+
+        state.next_id_ex.RegDst = false;
+        state.next_id_ex.RegWrite = false;
+        state.next_id_ex.ALUSrc = false;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
+    }
     else {
-        std::cerr << "Undocumented operator used. Skipping instruction.";
+        std::cerr << "Undocumented operator used. Passing NOP.";
+
+        op = "NOP";
+
+        state.next_id_ex.RegDst = false;
+        state.next_id_ex.RegWrite = false;
+        state.next_id_ex.ALUSrc = false;
+        state.next_id_ex.MemWrite = false;
+        state.next_id_ex.MemRead = false;
+        state.next_id_ex.MemtoReg = false;
+        state.next_id_ex.PCSrc = false;
     }
 
-    /*
-    else if (op == "beq") {
-        int rs = getR(a), rt = getR(b);
-        int offset = stoi(c);
+    state.next_id_ex.rsVal = rsVal;
+    state.next_id_ex.rtVal = rtVal;
+    state.next_id_ex.imm = imm;
+    state.next_id_ex.rs = rs;
+    state.next_id_ex.rt = rt;
+    state.next_id_ex.rd = rd;
+    state.next_id_ex.ALUOp = op;
+}
 
-        ctrl.Branch = 1;
+void EXStage(MachineState& state) {
+    state.ex_mem = state.next_ex_mem;
 
-        if (REG[rs] == REG[rt]) {
-            PC += offset;
-            continue;
+    int rsVal = state.id_ex.rsVal;
+    int rtVal = state.id_ex.rtVal;
+    int imm = state.id_ex.imm;
+    int rs = state.id_ex.rs;
+    int rt = state.id_ex.rt;
+    int rd = state.id_ex.rd;
+
+    std::string op = state.id_ex.ALUOp;
+
+    if(op == "add" || op == "Add" || op == "ADD") {
+
+        int sum = op_add(rsVal, rtVal);
+
+        state.next_ex_mem.ALUResult = sum;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "addi" || op == "Addi" || op == "ADDI") {
+
+        int sum = op_addi(rsVal, imm);
+
+        state.next_ex_mem.ALUResult = sum;
+        state.next_ex_mem.writeReg = rt;
+    }
+    else if(op == "sub" || op == "Sub" || op == "SUB") {
+
+        int diff = op_sub(rsVal, rtVal);
+
+        state.next_ex_mem.ALUResult = diff;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "mul" || op == "Mul" || op == "MUL") {
+
+        int prod = op_mul(rsVal, rtVal);
+
+        state.next_ex_mem.ALUResult = prod;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "and" || op == "And" || op == "AND") {
+
+        int result = op_and(rsVal, rtVal);
+
+        state.next_ex_mem.ALUResult = result;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "or" || op == "Or" || op == "OR") {
+
+        int result = op_or(rsVal, rtVal);
+
+        state.next_ex_mem.ALUResult = result;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "sll" || op == "Sll" || op == "SLL") {
+
+        int result = op_sll(rtVal, imm);
+
+        state.next_ex_mem.ALUResult = result;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "srl" || op == "Srl" || op == "SRL") {
+
+        int result = op_srl(rtVal, imm);
+
+        state.next_ex_mem.ALUResult = result;
+        state.next_ex_mem.writeReg = rd;
+    }
+    else if(op == "lw" || op == "Lw" || op == "LW") {
+
+        int address = op_lw(rs, imm);
+
+        state.next_ex_mem.ALUResult = address;
+        state.next_ex_mem.writeReg = rt;
+    }
+    else if(op == "sw" || op == "Sw" || op == "SW") {
+
+        int address = op_sw(rs, imm);
+
+        state.next_ex_mem.ALUResult = address;
+        state.next_ex_mem.writeData = rtVal;
+    }
+    else if(op == "beq" || op == "Beq" || op == "BEQ") {
+        
+        bool result = op_beq(rsVal, rtVal);
+
+        // Branch is taken, flush pipeline
+        if(result) {
+            state.next_if_id = {"NOP"};
+
+            state.next_id_ex = {0, 0, 0, -1, -1, -1,
+                false, false, false, false, false, false, false, "NOP"};
+
+            state.PC = imm;
         }
     }
-    else if (op == "j") {
-        int target = stoi(a);
-        state.PC = target;
-        continue;
+    else if(op == "j" || op == "J") {
+
+        // Flush pipeline for jump
+        state.next_if_id = {"NOP"};
+
+        state.next_id_ex = {0, 0, 0, -1, -1, -1,
+            false, false, false, false, false, false, false, "NOP"};
+
+        state.PC = imm;
     }
-    */
+    else if(op == "nop" || op == "Nop" || op == "NOP") {
+        // Do nothing
+    }
 
-    if(debug) {
-        std::cout << "Registers:" << std::endl;
+    state.next_ex_mem.RegDst = state.id_ex.RegDst;
+    state.next_ex_mem.RegWrite = state.id_ex.RegWrite;
+    state.next_ex_mem.ALUSrc = state.id_ex.ALUSrc;
+    state.next_ex_mem.MemWrite = state.id_ex.MemWrite;
+    state.next_ex_mem.MemRead = state.id_ex.MemRead;
+    state.next_ex_mem.MemtoReg = state.id_ex.MemtoReg;
+    state.next_ex_mem.PCSrc = state.id_ex.PCSrc;
+    state.next_ex_mem.ALUOp = state.id_ex.ALUOp;
+}
 
-        // Counter makes new line every four registers to make it look pretty
-        int counter = 1;
+void MEMStage(MachineState& state) {
+    state.mem_wb = state.next_mem_wb;
 
-        for(int i = 0; i < 32; i++) {
-            std::string reg;
+    if(state.ex_mem.MemWrite) {
+        state.MEM[state.ex_mem.ALUResult] = state.ex_mem.writeData;
+    }
 
-            for(const auto& pair : state.regMap) {
-                if(pair.second == i) {
-                    reg = pair.first;
+    if(state.ex_mem.MemRead) {
+        state.next_mem_wb.memData = state.MEM[state.ex_mem.ALUResult];
+    }
 
-                    break;
-                }
-            }
+    state.next_mem_wb.ALUResult = state.ex_mem.ALUResult;
+    state.next_mem_wb.writeReg = state.ex_mem.writeReg;
 
-            std::cout << std::setw(5) << "[" << reg << "]: " << state.REG[i];
+    state.next_mem_wb.RegWrite = state.ex_mem.RegWrite;
+    state.next_mem_wb.MemtoReg = state.ex_mem.MemtoReg;
+}
 
-            if(counter % 4 == 0) {
-                std::cout << std::endl;
-            }
-
-            counter++;
+void WBStage(MachineState& state) {
+    if(state.mem_wb.RegWrite) {
+        if(state.mem_wb.writeReg != 0) {
+            state.REG[state.mem_wb.writeReg] = state.mem_wb.ALUResult;
         }
+    }
 
-        std::cout << std::endl;
-        std::cout << "Control Signals:" << std::endl;
-        std::cout << "RegDst: " << state.ctrl[0] << std::endl;
-        std::cout << "RegWrite: " << state.ctrl[1] << std::endl;
-        std::cout << "ALUSrc: " << state.ctrl[2] << std::endl;
-        std::cout << "MemWrite: " << state.ctrl[3] << std::endl;
-        std::cout << "MemRead: " << state.ctrl[4] << std::endl;
-        std::cout << "MemtoReg: " << state.ctrl[5] << std::endl;
-        std::cout << "PCSrc: " << state.ctrl[6] << std::endl;
+    if(state.mem_wb.MemtoReg) {
+        if(state.mem_wb.writeReg != 0) {
+            state.REG[state.mem_wb.writeReg] = state.mem_wb.memData;
+        }
     }
 }
